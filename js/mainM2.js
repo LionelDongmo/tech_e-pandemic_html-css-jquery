5af13ba6da02aca2ceade30bcca05b370d0a06dc
LionelDongmo <51488249+LionelDongmo@users.noreply.github.com>
2020-12-17 22:29:25 +0300

    Add files via upload

---
+++ js/mainM2.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function() {
+    "user strict"
+    const responsive = {
+        0: {
+            items: 1
+        },
+        320: {
+            items: 1
+        },
+        560: {
+            items: 2
+        },
+        960: {
+            items: 3
+        }
+    }
+
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+    // click to scroll top
+    $('.move-up span').click(function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 1500);
+        })
+        //AOS Instance
+    AOS.init();
+});
